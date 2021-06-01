3788d2508b39af8e6ddb87923543698c50581d13
Vinitha3 <84272848+Vinitha3@users.noreply.github.com>
2021-06-01 19:38:51 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,60 @@
+console.log("hello");
+const number = Math.random ();
+console.log(number)
+const num = Math.random ()*100;
+console.log(num)
 
+console.log("1")
+console.log("2")
 
+let i = 1;
+while(i < 10){
+  console.log(i)
+  i = i + 1;
+}
 
+let j = 2;
+while(j <= 10){
+  console.log(j)
+  j = j + 1;
+}
 
+for (k=0; k<10; k=k+1){
+  console.log(k)
+}
+
+for (a=0; a<10; a=a+2){
+  console.log(a)
+}
+
+const floor = Math.floor(number);
+console.log(floor)
+
+if(floor%2 ==0){
+  console.log("even");
+}
+
+if(floor %2==0){
+  console.log("even");
+} else {
+  console.log("odd");
+} 
+
+let b = number;
+switch (b) {
+  case 5:
+    console.log("Buzz");
+    break;
+    case 0:
+      console.log("Zero");
+      break;
+      default:
+      console.log("Fizz");
+}
+
+for (let i = 0; i < 10; i++){
+  if (i==5){
+    break;
+  }
+  console.log(i);
+}
